b621dccbcf5b675836ece336cfbc3c496d8e9465
whchoi98 <whchoi98@gmail.com>
2024-12-25 15:43:40 +0000

    add tf routing module

--- my-project/envs/prod/main.tf
+++ my-project/envs/prod/main.tf
@@ -25,7 +25,7 @@ module "vpc" {
   ]
 }
 module "routing" {
-  source              = "../../modules/routing"
+  source              = "../../modules/routing-table"
   name                = "prod"
   environment         = "production"
   vpc_id              = module.vpc.vpc_id
