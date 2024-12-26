65f65d189d2e4ca788cdfca428d9429d24548f32
whchoi98 <whchoi98@gmail.com>
2024-12-26 01:43:24 +0000

    add tf module

--- my-project/envs/prod/variables.tf
+++ my-project/envs/prod/variables.tf
@@ -1,15 +1,18 @@
-# AWS 리전 변수 / AWS Region variable
+# AWS 리전 변수 / AWS Region Variable
 # 리소스가 배포될 AWS 리전을 정의합니다. / Defines the AWS region where resources will be deployed.
 variable "region" {
   description = "AWS Region" # AWS 리전
   type        = string       # 문자열 타입 / String type
+  default     = "ap-northeast-2" # 기본값: ap-northeast-2 / Default: ap-northeast-2
 }
 
-# 환경 변수 / Environment variable
-# 배포 환경 이름(dev, staging, prod)을 정의합니다. / Defines the deployment environment name (e.g., dev, staging, prod).
+# 환경 변수 / Environment Variable
+# 배포 환경 이름(dev, staging, prod 등)을 정의합니다.
+# Defines the deployment environment name (e.g., dev, staging, prod).
 variable "environment" {
   description = "Environment name (e.g., dev, staging, prod)" # 환경 이름 (예: dev, staging, prod)
   type        = string                                       # 문자열 타입 / String type
+  default     = "prod"                                       # 기본값: prod / Default: prod
 }
 
 # 공통 태그 변수 / Common tags variable
@@ -25,24 +28,24 @@ variable "common_tags" {
 
 # 퍼블릭 서브넷 ID 변수 / Public subnet IDs variable
 # 퍼블릭 서브넷의 ID 목록을 정의합니다. / Defines the list of public subnet IDs.
-variable "public_subnet_ids" {
-  description = "List of public subnet IDs" # 퍼블릭 서브넷 ID 목록
-  type        = list(string)               # 문자열 리스트 타입 / List of strings
-}
+#variable "public_subnet_ids" {
+#  description = "List of public subnet IDs" # 퍼블릭 서브넷 ID 목록
+#  type        = list(string)               # 문자열 리스트 타입 / List of strings
+#}
 
 # 프라이빗 서브넷 ID 변수 / Private subnet IDs variable
 # 프라이빗 서브넷의 ID 목록을 정의합니다. / Defines the list of private subnet IDs.
-variable "private_subnet_ids" {
-  description = "List of private subnet IDs" # 프라이빗 서브넷 ID 목록
-  type        = list(string)                # 문자열 리스트 타입 / List of strings
-}
+#variable "private_subnet_ids" {
+#  description = "List of private subnet IDs" # 프라이빗 서브넷 ID 목록
+#  type        = list(string)                # 문자열 리스트 타입 / List of strings
+#}
 
 # VPC ID 변수 / VPC ID variable
 # 리소스가 연결될 VPC의 ID를 정의합니다. / Defines the ID of the VPC to which resources are connected.
-variable "vpc_id" {
-  description = "VPC ID" # VPC ID
-  type        = string   # 문자열 타입 / String type
-}
+#variable "vpc_id" {
+#  description = "VPC ID" # VPC ID
+#  type        = string   # 문자열 타입 / String type
+#}
 
 # 스택 이름 변수 / Stack name variable
 # 스택의 이름을 정의합니다. / Defines the name of the stack.
