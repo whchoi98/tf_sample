ce4b25a56f8930a9af1b5f7d25e748b6ffcce357
whchoi98 <whchoi98@gmail.com>
2024-12-26 09:21:26 +0000

    add tf module

--- my-project/modules/iam/roles/outputs.tf
+++ my-project/modules/iam/roles/outputs.tf
@@ -16,7 +16,7 @@ output "ssm_instance_profile_arn" {
 
 # Output: SSM 인스턴스 프로파일 이름 / SSM Instance Profile Name
 # 생성된 SSM IAM 인스턴스 프로파일의 이름을 출력합니다.
-# Outputs the name of the created SSM IAM instance profile.
+# Outputs the name of the created SSM instance profile.
 output "ssm_instance_profile_name" {
   description = "Name of the SSM Instance Profile" # SSM 인스턴스 프로파일의 이름
   value       = aws_iam_instance_profile.ssm_instance_profile.name # SSM IAM 인스턴스 프로파일의 이름 / Name of the SSM IAM instance profile
@@ -36,10 +36,4 @@ output "ssm_role_policy_arns" {
 output "tags" {
   description = "Tags applied to IAM resources" # IAM 리소스에 적용된 태그
   value       = var.common_tags                 # 공통 태그 출력 / Common tags output
-}
-
-# Output: SSM IAM 역할 정책 ARN 목록 / List of SSM IAM Role Policy ARNs
-output "ssm_role_policy_arns" {
-  description = "List of ARNs of the policies attached to the SSM Role" # SSM 역할에 연결된 정책 ARN 목록
-  value       = [for attachment in aws_iam_role_policy_attachment.ssm_role : attachment.policy_arn] # 역할에 연결된 정책 ARN / List of policy ARNs attached to the role
 }
