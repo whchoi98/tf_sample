ad11ec57f833995cf59f1d5c4dadbde603ea6e0a
whchoi98 <whchoi98@gmail.com>
2024-12-29 07:09:46 +0000

    add db/elasticache

--- my-project/modules/db/outputs.tf
+++ my-project/modules/db/outputs.tf
@@ -1,29 +1,29 @@
 # Output: Aurora Cluster Endpoint
-output "aurora_cluster_endpoint" {
+output "cluster_endpoint" {
   description = "The endpoint of the Aurora MySQL cluster"
   value       = aws_rds_cluster.aurora.endpoint
 }
 
 # Output: Aurora Reader Endpoint
-output "aurora_reader_endpoint" {
+output "reader_endpoint" {
   description = "The reader endpoint of the Aurora MySQL cluster"
   value       = aws_rds_cluster.aurora.reader_endpoint
 }
 
 # Output: Aurora Cluster ID
-output "aurora_cluster_id" {
+output "cluster_id" {
   description = "The ID of the Aurora MySQL cluster"
   value       = aws_rds_cluster.aurora.id
 }
 
 # Output: Aurora Instance Endpoints
-output "aurora_instance_endpoints" {
+output "instance_endpoints" {
   description = "The endpoints of the Aurora MySQL instances"
-  value       = [for instance in aws_rds_cluster_instance.aurora_instances : instance.endpoint]
+  value       = [for instance in aws_rds_cluster_instance.instances : instance.endpoint]
 }
 
 # Output: Aurora Instance IDs
-output "aurora_instance_ids" {
+output "instance_ids" {
   description = "The IDs of the Aurora MySQL instances"
-  value       = [for instance in aws_rds_cluster_instance.aurora_instances : instance.id]
+  value       = [for instance in aws_rds_cluster_instance.instances : instance.id]
 }
