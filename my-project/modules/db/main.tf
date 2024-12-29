0449d0cfe63b9785a96945d6c4f67b6426725399
whchoi98 <whchoi98@gmail.com>
2024-12-29 07:11:53 +0000

    add db/elasticache

--- my-project/modules/db/main.tf
+++ my-project/modules/db/main.tf
@@ -52,3 +52,20 @@ resource "aws_db_subnet_group" "aurora" {
     }
   )
 }
+
+# RDS 클러스터 인스턴스 / RDS Cluster Instances
+resource "aws_rds_cluster_instance" "instances" {
+  count              = var.instance_count
+  identifier         = "${var.name}-instance-${count.index + 1}" # 각 인스턴스 식별자 / Instance identifier
+  cluster_identifier = aws_rds_cluster.aurora.id
+  instance_class     = var.instance_class
+  publicly_accessible = var.publicly_accessible
+  engine             = "aurora-mysql"
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.name}-instance-${count.index + 1}"
+    }
+  )
+}
